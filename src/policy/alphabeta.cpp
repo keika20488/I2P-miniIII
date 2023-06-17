602fbef6461f3f25877a187d215a6094d28125b8
keika20488 <emma990091@gmail.com>
2023-06-17 22:55:49 +0800

    AlphaBeta

--- src/policy/alphabeta.cpp
+++ src/policy/alphabeta.cpp
@@ -15,10 +15,37 @@ Move AlphaBeta::get_move(State *state, int depth){
   if(!state->legal_actions.size())
     state->get_legal_actions();
   
-  int k = depth;
-  auto actions = state->legal_actions;
-  for (int i=0; i<depth; i++){
-    k = rand()/k;
+  Move best;
+  int MAX = -__INT_MAX__, score;
+  for (auto move: state->legal_actions) {
+    score = alphabeta(state->next_state(move), depth, -__INT_MAX__, __INT_MAX__, true);
+    if (score > MAX) {
+      MAX = score;
+      best = move;
+    }
   }
-  return actions[k%actions.size()];
+  return best;
+}
+
+int AlphaBeta::alphabeta(State *state, int depth, int a, int b, bool maximize) {
+  if (!depth) return state->evaluate();
+  if (!state->legal_actions.size())
+    state->get_legal_actions();
+  int value;
+  if (maximize) {
+    value = -__INT_MAX__;
+    for (auto move: state->legal_actions) {
+      value = std::max(value, alphabeta(state->next_state(move), depth-1, a, b, false));
+      a = std::max(a, value);
+      if (a >= b) break;
+    }
+  } else {
+    value = __INT_MAX__;
+    for (auto move: state->legal_actions) {
+      value = std::min(value, alphabeta(state->next_state(move), depth-1, a, b, false));
+      b = std::min(b, value);
+      if (b <= a) break;
+    }
+  }
+  return value;
 }
