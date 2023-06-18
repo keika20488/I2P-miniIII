1dce378760eb3f8aed1c3f71749d7227092c0b0a
keika20488 <emma990091@gmail.com>
2023-06-18 22:03:00 +0800

    correct maximize value in get_move

--- src/policy/minimax.cpp
+++ src/policy/minimax.cpp
@@ -14,11 +14,11 @@
 Move Minimax::get_move(State *state, int depth){
   if (!state->legal_actions.size())
     state->get_legal_actions();
-  
+
   Move best;
-  int MAX = -__INT_MAX__, score;
+  int score, MAX = -__INT_MAX__;
   for (auto move: state->legal_actions) {
-    score = minimax(state->next_state(move), depth, true);
+    score = minimax(state->next_state(move), depth-1, false);
     if (score > MAX) {
       MAX = score;
       best = move;
@@ -28,18 +28,32 @@ Move Minimax::get_move(State *state, int depth){
 }
 
 int Minimax::minimax(State *state, int depth, bool maximize) {
-  if (!depth) return state->evaluate();
   if (!state->legal_actions.size())
     state->get_legal_actions();
+
+  if (state->legal_actions.empty()) {
+    if (state->game_state == WIN)
+      return maximize ? __INT_MAX__ : -__INT_MAX__;
+    else return maximize ? -__INT_MAX__ : __INT_MAX__;
+  }
+
+  if (!depth) return state->evaluate();
+
   int value;
+  auto actions = state->legal_actions;
+  State *next;
   if (maximize) {
     value = -__INT_MAX__;
-    for (auto move: state->legal_actions)
-      value = std::max(value, minimax(state->next_state(move), depth-1, false));
+    for (auto move: actions){
+      next = state->next_state(move);
+      value = std::max(value, minimax(next, depth-1, false));
+    }
   } else {
     value = __INT_MAX__;
-    for (auto move: state->legal_actions)
-      value = std::min(value, minimax(state->next_state(move), depth-1, true));
+    for (auto move: actions){
+      next = state->next_state(move);
+      value = std::min(value, minimax(next, depth-1, true));
+    }
   }
   return value;
 }
--- src/policy/minimax.hpp
+++ src/policy/minimax.hpp
@@ -6,6 +6,7 @@
  * @brief Policy class for random policy, 
  * your policy class should have get_move method
  */
+const int MAX_DEPTH = 10;
 class Minimax{
 public:
   static Move get_move(State *state, int depth);
